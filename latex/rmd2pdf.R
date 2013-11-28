3753b45e884d6fc99158d5335ea4833867f3a8ba
Robin Lovelace <rob00x@gmail.com>
2013-11-28 16:23:10 +0000

    all figs, intro
    Former-commit-id: a41c9f21427e49db39e344c236cd8e9f9b86bd77

--- latex/rmd2pdf.R
+++ latex/rmd2pdf.R
@@ -3,11 +3,27 @@ mess <- paste('pandoc -f markdown -t latex -s -o', "intro-spatial.tex",
               "intro-spatial.md")
 system(mess) # create latex file
 
-mess2 <- paste("sed 's/plot of.chunk.//g' intro-spatial.tex > intro-spatial-rl.tex")
-system(mess2) # replace "plot of chunk " text with nowth
+mess <- paste("sed 's/plot of.chunk.//g' intro-spatial.tex > intro-spatial-rl.tex")
+system(mess) # replace "plot of chunk " text with nowth
 
-mess <- paste("sed -i -e 's/.maxwidth/8cm/g' intro-spatial-rl.tex")
+mess <- paste("sed -i -e 's/width=\\\\maxwidth/width=8cm/g' intro-spatial-rl.tex")
 system(mess)
+
+mess <- "sed -i -e '62i\\\\\\usepackage[margin=2cm]{geometry}' intro-spatial-rl.tex"
+system(mess)
+
+# stop here
+
+mess <- "sed -i -e '62i\\\\author{
+   Cheshire, James\\
+  \texttt{james.cheshire@ucl.ac.uk}
+  \and
+  Lovelace, Robin\\
+  \texttt{r.lovelace@leeds.ac.uk}
+}
+\title{Introduction to Spatial Data and ggplot2}' intro-spatial-rl.tex"
+system(mess)
+
 
 # from http://conjugateprior.org/2012/12/r-markdown-to-other-document-formats/
 
