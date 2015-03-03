10c0a9ca30890e76dabfcbe257f5255d50cc22f1
Robinlovelace <rob00x@gmail.com>
2015-03-03 11:17:55 +0000

    Update to project files

--- vignettes/leaflet-demo/server.R
+++ vignettes/leaflet-demo/server.R
@@ -6,7 +6,8 @@ function(input, output) {
 #   map = leaflet() %>% addTiles() %>% # basic map background
   
 #   opencyclemap background:
-  map = leaflet() %>% addTiles(http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png) %>%
+#   see http://leaflet-extras.github.io/leaflet-providers/preview/index.html
+  map = leaflet() %>% addTiles("http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png") %>%
       setView(-1.5, 53.4, zoom = 10) %>% # map location
       addMarkers(-1.4, 53.5) %>% # add a marker
       addPopups(-1.6, 53.3, popup = "Hello Sheffield!") %>% # popup
