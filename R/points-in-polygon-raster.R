f0a6d3a0fdf7dc2594273bc6c28dc6519bed9d34
Robin <rob00x@gmail.com>
2015-12-18 16:23:16 +0000

    Add points in polygon script

---
+++ R/points-in-polygon-raster.R
@@ -0,0 +1,22 @@
+pkgs <- c("geojsonio", "raster")
+lapply(pkgs, library, character.only = T)
+
+url <- "https://raw.githubusercontent.com/glynnbird/usstatesgeojson/master/wyoming.geojson"
+download.file(url, destfile = "wyoming.geojson")
+x <- geojson_read("wyoming.geojson", what = "sp") # or what=sp if you want a spatial object
+plot(x)
+
+# generate random data to sample from
+
+rpoints <- spsample(x = x, n = 100, type = "random")
+points(rpoints)
+rpoints <- SpatialPointsDataFrame(rpoints,
+                                  data = data.frame(v = rep(1, length(rpoints))))
+
+r <- raster(x = x, ncols = 3, nrows = 3)
+r <- rasterize(rpoints, y = r, field = "v", fun = "count")
+xras <- rasterise(x = x, y = r, ext = extent(x))
+
+plot(r)
+plot(x, add = T)
+points(rpoints)
