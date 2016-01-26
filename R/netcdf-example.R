dd1884d8c02e3e703e0a7c1bdcfbbdaf4e3b0bcf
robinlovelace <rob00x@gmail.com>
2016-01-26 16:10:55 +0000

    Add netcdf example

---
+++ R/netcdf-example.R
@@ -0,0 +1,11 @@
+# Aim: load and visualise netcdf data
+
+# download data from nasa, as described here:
+# http://disc.sci.gsfc.nasa.gov/recipes/?q=recipes/How-to-Read-Data-in-netCDF-Format-with-R
+
+# install ncdf4
+# install.packages("ncdf4")
+
+# load the data
+r <- raster("data/nc_3B42.20060101.03.7A.HDF.Z.ncml.nc")
+plot(r)
