260ec8059399767479333a31bd52cb5a27ce00d2
robinlovelace <rob00x@gmail.com>
2016-09-21 06:19:48 +0100

    Add simple features minibench

---
+++ R/sfr-minibench.R
@@ -0,0 +1,7 @@
+old = setwd("/home/robin/repos/Creating-maps-in-R/")
+f = "data"
+rgdal_way = function() readOGR(dsn = f, layer = "LondonBoroughs", verbose = FALSE)
+sfr_way = function() st_read(dsn = f, layer = "LondonBoroughs", quiet = TRUE)
+microbenchmark(rgdal_way(), sfr_way(), times = 3)
+lnd_sfr = st_read(dsn = f, layer = "LondonBoroughs", quiet = TRUE)
+plot(lnd_sfr)
